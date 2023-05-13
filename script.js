549af815b8d074f7c9b8aa0d495fd12d6555a88f
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-13 09:13:22 +0000

    P2R

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  const ans = arr.map((e) => {
+    if(e.marks > 50)console.log(e);
+  });
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  for(let student in arr){
+    if(arr[student].marks > 50){
+      console.log(arr[student]);
+    }
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let students = {id:4,name:"susan",age:"20",marks:45}
+  arr.push(students)
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  // let students = [];
+  // for(let i in arr){
+  //   if(arr[i].marks > 50) {
+  //      students.push(arr[i]);
+  //   }
+  
+  // }
+  // // arr = students;
+  arr = arr.filter(e => (e.marks > 50));
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let students = [
+    {id: 5, name: "Mon", age: "15", marks: 50 },
+    {id: 6, name: "Ton", age: "22", marks: 43 },
+    {id: 7, name: "Lon", age: "24", marks: 100 }
+  ]
+  arr=arr.concat(students)
+  console.log(arr);
 }
